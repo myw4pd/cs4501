d3df08e8c78d45bbde33af05d4a5ddb4f3fe73f2
agnesskim <ak5nt@virginia.edu>
2016-09-19 23:20:24 -0400

    update

--- app/project/review/models.py
+++ app/project/review/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
+from django.utils import timezone
+
 # Create your models here.
+class CarReview(models.Model):
+    title = models.CharField(max_length=255)
+    car_id = models.CharField(max_length=20)
+    user_id = models.ForeignKey('User')
+    rating = models.CharField(max_length=20)
+    text = models.TextField()
+    date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
+
+class UserReview(models.Model):
+    title = models.CharField(max_length=255)
+    user_id = models.ForeignKey('User')
+    text = models.CharField(max_length=200)
+    date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+         return self.title
--- app/project/review/views.py
+++ app/project/review/views.py
@@ -1,3 +1,72 @@
 from django.shortcuts import render
 
+from .models import UserReview
+from .models import CarReview
+from review import models
+import datetime
+
+from django.http import JsonResponse
+from django.contrib.auth import hashers
+from django.forms.models import model_to_dict
+from django import db
+
+
 # Create your views here.
+def create_CarReview(request):
+    if request.method != POST:
+        return _error_response(request, "must make POST request")
+    if 'title' not in request.POST or       \
+        'car_id' not in request.POST or     \
+        'user_id' not in request.POST or    \
+        'rating' not in request.POST or     \
+        'text' not in request.POST:
+        return _error_response(request, "missing required fields")
+
+    c = models.UserReview(title=request.POST['title'], \
+                           car_id=request.POST['car_id'], \
+                            user_id = request.POST['user_id'],\
+                           rating = request.POST['rating'], \
+                           text = request.POST['text'], \
+                           date = request.datetime.datetime.now() \
+                           )
+
+    try:
+        u.save()
+    except db.Error:
+        return _error_response(request, "db error")
+
+    return _success_response(request, {'title': c.pk})
+
+
+def create_UserReview(request):
+    if request.method != POST:
+        return _error_response(request, "must make POST request")
+    if 'title' not in request.POST or       \
+        'user_id' not in request.POST or    \
+        'text' not in request.POST:
+        return _error_response(request, "missing required fields")
+
+    ur = models.UserReview(title=request.POST['title'], \
+                            user_id = request.POST['user_id'],\
+                           text = request.POST['text'], \
+                           date = request.datetime.datetime.now() \
+                           )
+    try:
+        u.save()
+    except db.Error:
+        return _error_response(request, "db error")
+
+    return _success_response(request, {'title': ur.pk})
+
+
+
+#helper functions
+
+def _error_response(request, error_msg):
+    return JsonResponse({'ok': False, 'error': error_msg})
+
+def _success_response(request, resp=None):
+    if resp:
+        return JsonResponse({'ok': True, 'resp': resp})
+    else:
+        return JsonResponse({'ok': True})
